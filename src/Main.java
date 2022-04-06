f626f08f83d5816b70010f6202370028cf29bd9b
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2022-04-06 18:23:58 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         Ukoly u = new Ukoly();
-        //System.out.println(u.avg());
-        //u.kame();
+        System.out.println(u.avg());
+        u.kame();
         u.gallow();
 
 
--- src/Ukoly.java
+++ src/Ukoly.java
@@ -1,75 +1,88 @@
 import java.util.*;
 
 public class Ukoly {
-    double avg(){
+    double avg() {
         Scanner sc = new Scanner(System.in);
         double output = 0;
         ArrayList<Double> nums = new ArrayList<>();
-        while(true){
+        while (true) {
             System.out.println("Gib num pls. Kill it with end");
             String gibbedNum = sc.nextLine();
-            if(!Objects.equals(gibbedNum, "end")){
+            if (!Objects.equals(gibbedNum, "end")) {
                 nums.add(Double.parseDouble(gibbedNum));
                 output += Double.parseDouble(gibbedNum);
-            }
-            else{
+            } else {
                 break;
             }
         }
         return (output / nums.toArray().length);
     }
-    void kame(){
+
+    void kame() {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
         int points = 0;
         String ai;
-        while(points < 10){
+        while (points < 10) {
             System.out.println("KOlo začíná");
             System.out.println("Na výběr máš: k,n,p");
             String input = sc.nextLine();
-            switch(rand.nextInt(4)){
+            switch (rand.nextInt(4)) {
                 case 0 -> ai = "k";
                 case 1 -> ai = "n";
                 default -> ai = "p";
             }
-            if(ai.equals(input)){
+            if (ai.equals(input)) {
                 System.out.println("Remíza");
-            }
-            else if(ai.equals("k") && input.equals("p") || ai.equals("n") && input.equals("k") || ai.equals("p") && input.equals("k")){
+            } else if (ai.equals("k") && input.equals("p") || ai.equals("n") && input.equals("k") || ai.equals("p") && input.equals("k")) {
                 System.out.println("Vyhráváš. +1 bod");
                 points++;
-            }
-            else{
+            } else {
                 System.out.println("Prohráváš. -1 bod");
                 points--;
             }
         }
     }
-    void gallow(){
+
+    void gallow() {
         Random r = new Random();
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
         int lives = 5;
         String[] words = {"ahoj", "pepa", "sus", "amogus"};
-        char[] word = words[r.nextInt(words.length)].toCharArray();
-        ArrayList<String> chars = new ArrayList<>();
-        while(lives > 0) {
-            System.out.println("Hádáš. Máš " + lives + " životy.");
-            System.out.println("Slovo je: ");
-            String output = "";
-            for (char c : word) {
-                for (int j = 0; j < chars.toArray().length; j++) {
-                    if (c == chars.get(j).toCharArray()[j]) {
-                        output += chars;
+        String word = words[r.nextInt(words.length)];
+        StringBuilder usedChars = new StringBuilder("");
+
+        while (true) {
+            StringBuilder showWord = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                boolean wasThere = false;
+                for (int j = 0; j < usedChars.length(); j++) {
+                    if (word.charAt(i) == usedChars.charAt(j)) {
+                        showWord.append(usedChars.charAt(j));
+                        wasThere = true;
                         break;
                     }
                 }
-                output += "*";
+                if (!wasThere) {
+                    showWord.append("*");
+                }
             }
-            System.out.println("Zadej pismeno");
-            chars.add(sc.nextLine());
-            if(output){
-
+            System.out.println("Slovo je: " + showWord);
+            if (showWord.toString().equals(word)) {
+                System.out.println("U won");
+                break;
+            } else {
+                if (!usedChars.isEmpty()) {
+                    System.out.println("Wrong letter");
+                    lives--;
+                    if (lives <= 0) {
+                        System.out.println("U ded");
+                        break;
+                    }
+                }
             }
+            System.out.println("Zadej písmeno");
+            usedChars.append(sc.nextLine());
         }
     }
 }
